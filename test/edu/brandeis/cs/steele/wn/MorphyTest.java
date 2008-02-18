7dbe1e74024d5be647448975f315db6cf2ab388c
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-02-18 05:48:29 +0000

    upgraded build system to something based on Lucene build - generates javadoc and runs unit tests - missing browser and example though
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@12 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

---
+++ test/edu/brandeis/cs/steele/wn/MorphyTest.java
@@ -0,0 +1,11 @@
+package edu.brandeis.cs.steele.wn;
+
+import junit.framework.TestCase;
+
+public class MorphyTest extends TestCase {
+  public void test1() {
+    System.out.println("some out");
+    System.err.println("some err");
+    assertTrue(true);
+  }
+}
