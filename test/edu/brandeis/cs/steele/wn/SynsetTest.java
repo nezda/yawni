550a9ad3d3c67f607856c74d2684fc34ccec681f
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-02-23 21:42:32 +0000

    added unit test
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@34 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

---
+++ test/edu/brandeis/cs/steele/wn/SynsetTest.java
@@ -0,0 +1,26 @@
+package edu.brandeis.cs.steele.wn;
+
+import junit.framework.JUnit4TestAdapter;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class SynsetTest {
+  @Test
+  public void test1() {
+    final DictionaryDatabase dictionary = FileBackedDictionary.getInstance();
+    for(final POS pos : POS.CATS) {
+      int count = 0;
+      for(final Synset synset : dictionary.synsets(pos)) {
+        if(++count > 10) break;
+        // exercise toString() and getGloss()
+        final String msg = count+" "+synset+"\n  "+synset.getGloss();
+        System.err.println(msg);
+      }
+    }
+  }
+
+  public static junit.framework.Test suite() {
+    return new JUnit4TestAdapter(SynsetTest.class);
+  }
+}
