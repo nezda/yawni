65a93164aff463c09bd529341a9e93f5a4fd75f1
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-05-24 00:48:44 +0000

    useful gui util
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@193 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- src/edu/brandeis/cs/steele/wn/browser/MoveMouseListener.java
+++ src/edu/brandeis/cs/steele/wn/browser/MoveMouseListener.java
@@ -10,48 +10,47 @@ import javax.swing.*;
  * @author http://www.oreilly.com/catalog/swinghks/
  */
 class MoveMouseListener implements MouseListener, MouseMotionListener {
-  JComponent target;
-  Point start_drag;
-  Point start_loc;
+  private final JComponent target;
+  private Point start_drag;
+  private Point start_loc;
 
-  public MoveMouseListener(JComponent target) {
+  public MoveMouseListener(final JComponent target) {
     this.target = target;
   }
 
-  public static JFrame getFrame(Container target) {
+  Point getScreenLocation(final MouseEvent evt) {
+    final Point cursor = evt.getPoint();
+    final Point target_location = this.target.getLocationOnScreen();
+    return new Point(
+        (int)(target_location.getX() + cursor.getX()),
+        (int)(target_location.getY() + cursor.getY()));
+  }
+
+  public void mouseClicked(final MouseEvent evt) {}
+  public void mouseEntered(final MouseEvent evt) {}
+  public void mouseExited(final MouseEvent evt) {}
+  public void mousePressed(final MouseEvent evt) {
+    this.start_drag = this.getScreenLocation(evt);
+    this.start_loc = this.getFrame(this.target).getLocation();
+  }
+  public void mouseReleased(final MouseEvent evt) {}
+  public void mouseDragged(final MouseEvent evt) {
+    final Point current = this.getScreenLocation(evt);
+    final Point offset = new Point(
+        (int)current.getX() - (int)start_drag.getX(),
+        (int)current.getY() - (int)start_drag.getY());
+    final JFrame frame = this.getFrame(target);
+    final Point new_location = new Point(
+        (int)(this.start_loc.getX() + offset.getX()),
+        (int)(this.start_loc.getY() + offset.getY()));
+    frame.setLocation(new_location);
+  }
+  public void mouseMoved(final MouseEvent evt) {}
+
+  static JFrame getFrame(final Container target) {
     if(target instanceof JFrame) {
       return (JFrame)target;
     }
-    return getFrame(target.getParent());
+    return MoveMouseListener.getFrame(target.getParent());
   }
-
-  Point getScreenLocation(MouseEvent e) {
-    Point cursor = e.getPoint();
-    Point target_location = this.target.getLocationOnScreen();
-    return new Point(
-        (int)(target_location.getX()+cursor.getX()),
-        (int)(target_location.getY()+cursor.getY()));
-  }
-  public void mouseClicked(MouseEvent e) {}
-  public void mouseEntered(MouseEvent e) {}
-  public void mouseExited(MouseEvent e) {}
-  public void mousePressed(MouseEvent e) {
-    this.start_drag = this.getScreenLocation(e);
-    this.start_loc = this.getFrame(this.target).getLocation();
-  }
-
-  public void mouseReleased(MouseEvent e) {}
-
-  public void mouseDragged(MouseEvent e) {
-    Point current = this.getScreenLocation(e);
-    Point offset = new Point(
-        (int)current.getX()-(int)start_drag.getX(),
-        (int)current.getY()-(int)start_drag.getY());
-    JFrame frame = this.getFrame(target);
-    Point new_location = new Point(
-        (int)(this.start_loc.getX()+offset.getX()),
-        (int)(this.start_loc.getY()+offset.getY()));
-    frame.setLocation(new_location);
-  }
-  public void mouseMoved(MouseEvent e) {}
-} // end class MoveMouseListener
+}
