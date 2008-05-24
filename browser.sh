fe7410fc66373cd0da801802f6cc08863bd5b40f
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-05-24 03:12:01 +0000

    git-svn-id: file:///Users/nezda/code/svn/jwordnet@202 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- browser.sh
+++ browser.sh
@@ -4,5 +4,9 @@ VERSION=1.5.6-dev
 #LEVEL_ARG=-Dedu.brandeis.cs.steele.wn.Morphy.level=SEVERE
 #ASSERT_ENABLE="-ea -esa"
 # system assertions cause tons of logging on OS X ([AWT-\d+] ...)
-ASSERT_ENABLE="-ea"
+#ASSERT_ENABLE="-ea -Xrunhprof:cpu=samples,depth=20,interval=20"
+#ASSERT_ENABLE="-ea"
+#-Dswing.aatext=true
+# the OS X brushMetal* stuff only seems to work if provided as a system property at the command line
+ASSERT_ENABLE="-ea -Dswing.aatext=true -Dawt.useSystemAAFontSettings=on -Dapple.awt.textantialiasing=on -Dapple.laf.useScreenMenuBar=true -Dapple.awt.brushMetalLook=true -Dapple.awt.brushMetalRounded=true -Dapple.awt.showGrowBox=false"
 java -Xdock:name="JWordNet Browser" $ARGS $LEVEL_ARG $ARGS -Dfile.encoding=US-ASCII $ASSERT_ENABLE -DWNHOME="$WNHOME" -cp ./build/jwordnet-core-$VERSION.jar edu.brandeis.cs.steele.wn.browser.Browser "$@"
