116c0956e7b8aebac24d2454c8f096b4b2a3dfd2
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-06-09 03:15:38 +0000

    comments
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@255 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- src/main/java/edu/brandeis/cs/steele/util/MutatedIterable.java
+++ src/main/java/edu/brandeis/cs/steele/util/MutatedIterable.java
@@ -1,24 +1,22 @@
 package edu.brandeis.cs.steele.util;
 
-import java.util.*;
+import java.util.Iterator;
 
 /**
  * Derive a new <code>Iterable</code> by calling a method on each of the base
- * <code>Iterable</code>'s <code>Iterator</code>'s items as computed by
- * implementations of the {@link #apply()} method.
+ * <code>Iterable</code>s' <code>Iterator</code>'s items as computed by
+ * implementations of the {@link #apply} method.
  */
 public abstract class MutatedIterable<T, R> implements Iterable<R> {
   private static final long serialVersionUID = 1L;
   private final Iterable<T> base;
-  private final Class<R> clazz;
-  public MutatedIterable(final Iterable<T> base, final Class<R> clazz) {
+  public MutatedIterable(final Iterable<T> base) {
     this.base = base;
-    this.clazz = clazz;
   }
   
   abstract public R apply(final T t);
 
-  @SuppressWarnings(value = "unchecked")
+  @SuppressWarnings("unchecked")
   public Iterator<R> iterator() {
     // i fought the compiler and the compiler won
     return (Iterator<R>)new MutatedIterator(base.iterator());
