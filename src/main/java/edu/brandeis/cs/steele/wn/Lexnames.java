c1d5fbafec22a27a2f1246e75d53dafc2e1a1142
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-07-11 00:12:55 +0000

    descriptive comments
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@344 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- src/main/java/edu/brandeis/cs/steele/wn/Lexnames.java
+++ src/main/java/edu/brandeis/cs/steele/wn/Lexnames.java
@@ -1,55 +1,59 @@
 package edu.brandeis.cs.steele.wn;
 
-/** 
- * Contents of (WordNet 3.0) lexnames file as String
- * arrays since it is "optional".
+/**
+ * Contents of (WordNet 3.0) lexnames file as String arrays since it is
+ * "optional".  These are the WordNet lexicographers classes, sometimes called
+ * the WordNet 'supersenses'.
+ * <cite>M. Ciaramita, Y. Altun. 2006. Broad-Coverage Sense Disambiguation and Information Extraction with a Supersense Sequence Tagger</cite>
+ * Supersense Tagging of Unknown Nouns in WordNet
+ * TODO integrate descriptions into API
  */
 class Lexnames {
   static final String contents [][] = {
     { "00", "adj.all", "3" },
     { "01", "adj.pert", "3 " },
     { "02", "adv.all", "4" },
-    { "03", "noun.Tops", "1  " },
-    { "04", "noun.act", "1" },
-    { "05", "noun.animal", "1" },
-    { "06", "noun.artifact", "1" },
-    { "07", "noun.attribute", "1" },
-    { "08", "noun.body", "1" },
-    { "09", "noun.cognition", "1" },
-    { "10", "noun.communication", "1" },
-    { "11", "noun.event", "1" },
-    { "12", "noun.feeling", "1" },
-    { "13", "noun.food", "1" },
-    { "14", "noun.group", "1" },
-    { "15", "noun.location", "1" },
-    { "16", "noun.motive", "1" },
-    { "17", "noun.object", "1" },
-    { "18", "noun.person", "1" },
-    { "19", "noun.phenomenon", "1" },
-    { "20", "noun.plant", "1" },
-    { "21", "noun.possession", "1" },
-    { "22", "noun.process", "1" },
-    { "23", "noun.quantity", "1" },
-    { "24", "noun.relation", "1" },
-    { "25", "noun.shape", "1" },
-    { "26", "noun.state", "1" },
-    { "27", "noun.substance", "1" },
-    { "28", "noun.time", "1" },
-    { "29", "verb.body", "2" },
-    { "30", "verb.change", "2" },
-    { "31", "verb.cognition", "2" },
-    { "32", "verb.communication", "2" },
-    { "33", "verb.competition", "2" },
-    { "34", "verb.consumption", "2" },
-    { "35", "verb.contact", "2" },
-    { "36", "verb.creation", "2" },
-    { "37", "verb.emotion", "2" },
-    { "38", "verb.motion", "2" },
-    { "39", "verb.perception", "2" },
-    { "40", "verb.possession", "2" },
-    { "41", "verb.social", "2" },
-    { "42", "verb.stative", "2" },
-    { "43", "verb.weather", "2" },
+    { "03", "noun.Tops", "1  " }, // abstract terms for unique beginners
+    { "04", "noun.act", "1" }, // acts or actions
+    { "05", "noun.animal", "1" }, // animals
+    { "06", "noun.artifact", "1" }, // man-made objects
+    { "07", "noun.attribute", "1" }, // attributes of people and objects
+    { "08", "noun.body", "1" }, // body parts
+    { "09", "noun.cognition", "1" }, // cognitive processes and contents
+    { "10", "noun.communication", "1" }, // communicative processes and contents
+    { "11", "noun.event", "1" }, // natural events
+    { "12", "noun.feeling", "1" }, // feelings and emotions
+    { "13", "noun.food", "1" }, // foods and drinks
+    { "14", "noun.group", "1" }, // groupings of people or objects
+    { "15", "noun.location", "1" }, // spatial position
+    { "16", "noun.motive", "1" }, // goals
+    { "17", "noun.object", "1" }, // natural objects (not man-made)
+    { "18", "noun.person", "1" }, // people
+    { "19", "noun.phenomenon", "1" }, // natural phenomena
+    { "20", "noun.plant", "1" }, // plants
+    { "21", "noun.possession", "1" }, // possession and transfer of possession
+    { "22", "noun.process", "1" }, // natural processes
+    { "23", "noun.quantity", "1" }, // quantities and units of measure
+    { "24", "noun.relation", "1" }, // relations between people or things or ideas
+    { "25", "noun.shape", "1" }, // two and three dimensional shapes
+    { "26", "noun.state", "1" }, // stable states of affairs
+    { "27", "noun.substance", "1" }, // substances
+    { "28", "noun.time", "1" }, // time and temporal relations
+    { "29", "verb.body", "2" }, // grooming, dressing and bodily care
+    { "30", "verb.change", "2" }, // size, temperature change, intensifying
+    { "31", "verb.cognition", "2" }, // thinking, judging, analyzing, doubting
+    { "32", "verb.communication", "2" }, // telling, asking, ordering, singing
+    { "33", "verb.competition", "2" }, // ﬁghting, athletic activities
+    { "34", "verb.consumption", "2" }, // eating and drinking
+    { "35", "verb.contact", "2" }, // touching, hitting, tying, digging
+    { "36", "verb.creation", "2" }, // sewing, baking, painting, performing
+    { "37", "verb.emotion", "2" }, // feeling
+    { "38", "verb.motion", "2" }, // walking, ﬂying, swimming
+    { "39", "verb.perception", "2" }, // seeing, hearing, feeling
+    { "40", "verb.possession", "2" }, // buying, selling, owning
+    { "41", "verb.social", "2" }, // political and social activities and events
+    { "42", "verb.stative", "2" }, // being, having, spatial relations
+    { "43", "verb.weather", "2" }, // raining, snowing, thawing, thundering
     { "44", "adj.ppl", "3" },
   };
 }
