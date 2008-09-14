84e16c310dd28eee7c513f1f3b97df7e56f612af
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2008-09-14 21:56:53 +0000

    minor cleanup
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@381 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- src/test/java/edu/brandeis/cs/steele/wn/ThreadSafetyTest.java
+++ src/test/java/edu/brandeis/cs/steele/wn/ThreadSafetyTest.java
@@ -2,6 +2,7 @@ package edu.brandeis.cs.steele.wn;
 
 import junit.framework.JUnit4TestAdapter;
 import org.junit.*;
+import static org.junit.Assert.*;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -16,33 +17,45 @@ public class ThreadSafetyTest {
     private final int id;
     private final Semaphore semaphore;
 
+    private int wordsVisited;
+    private int wordSensesVisited;
+    private int pointersVisited;
+
     Antagonizer(final int id, final Semaphore semaphore) { 
       super("Antagonizer "+id);
       this.id = id;
       this.semaphore = semaphore;
     }
 
+    boolean sameCounts(final Antagonizer that) {
+      return 
+        this.wordsVisited == that.wordsVisited &&
+        this.wordSensesVisited == that.wordSensesVisited &&
+        this.pointersVisited == that.pointersVisited;
+    }
+    @Override public String toString() {
+      return "[Antagonizer: " + id +
+        " wordsVisited: " + wordsVisited +
+        " wordSensesVisited: " + wordSensesVisited +
+        " pointersVisited: " + pointersVisited + "]";
+    }
+
     protected void antagonize() {
-      System.err.println(id+"  "+Thread.currentThread()+" starting...");
+      System.err.println(Thread.currentThread()+" starting...");
       final DictionaryDatabase dictionary = FileBackedDictionary.getInstance();
-      // iterate through Synset's of dictionary
-      // iterate through XXX
-      int wordsVisited = 0;
-      int pointersVisited = 0;
-      int indexWordsVisited = 0;
       try {
-        for(final POS pos : POS.CATS) {
-          for(final Word word : dictionary.words(pos)) {
-            ++indexWordsVisited;
-            for(final WordSense wordSense : word.getSenses()) {
+        for (final POS pos : POS.CATS) {
+          for (final Word word : dictionary.words(pos)) {
+            ++wordsVisited;
+            for (final WordSense wordSense : word.getSenses()) {
               final String lemma = wordSense.getLemma();
               final Synset synset = wordSense.getSynset();
               String msg = id+" "+wordSense;
               //System.err.println(msg);
-              ++wordsVisited;
+              ++wordSensesVisited;
             }
-            for(final Synset synset : word.getSynsets()) {
-              for(final Pointer pointer : synset.getPointers()) {
+            for (final Synset synset : word.getSynsets()) {
+              for (final Pointer pointer : synset.getPointers()) {
                 pointer.getTarget();
                 // note these are not unique - they are visited from both sides
                 ++pointersVisited;
@@ -51,9 +64,7 @@ public class ThreadSafetyTest {
           }
         }
       } finally {
-        System.err.println("Antagonizer: "+id+
-            " wordsVisited: "+wordsVisited+" indexWordsVisited: "+indexWordsVisited+
-            " pointersVisited: "+pointersVisited);
+        System.err.println(this);
       }
     }
 
@@ -64,7 +75,7 @@ public class ThreadSafetyTest {
         semaphore.release();
       }
     }
-  }
+  } // end class Antagonizer
 
   @Test
   public void test1() {
@@ -74,13 +85,13 @@ public class ThreadSafetyTest {
     //   release permit (n times: implies 1 permit remains after last completion)
     // acquire 1 permit
     // 3 Antagonizer's takes a while to run
-    final int numAntagonizers = 4;
+    final int numAntagonizers = 3;
     final Semaphore finisher = new Semaphore(1 - numAntagonizers, true /* fair */);
     final Antagonizer[] antagonizers = new Antagonizer[numAntagonizers];
-    for(int i=0; i<numAntagonizers; ++i) {
+    for (int i = 0; i < numAntagonizers; i++) {
       antagonizers[i] = new Antagonizer(i, finisher);
     }
-    for(final Antagonizer antagonizer : antagonizers) {
+    for (final Antagonizer antagonizer : antagonizers) {
       antagonizer.start();
     }
     // re-acquire all permits and exit
@@ -88,6 +99,9 @@ public class ThreadSafetyTest {
       finisher.acquire();
     } catch(InterruptedException ie) {
       throw new RuntimeException(ie);
+    }
+    for (int i = 1; i < numAntagonizers; i++) {
+      assertTrue(antagonizers[0].sameCounts(antagonizers[i]));
     }
     System.err.println("done");
   }
