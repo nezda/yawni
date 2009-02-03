a3f089e963e4add4cc406e4f05b521c6b44db143
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-03 03:18:47 +0000

    - added Apache license
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@500 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/main/java/edu/brandeis/cs/steele/wn/Pointer.java
+++ core/src/main/java/edu/brandeis/cs/steele/wn/Pointer.java
@@ -1,4 +1,20 @@
 /*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+/*
  * Copyright 1998 by Oliver Steele.  You can use this software freely so long as you preserve
  * the copyright notice and this restriction, and label your changes.
  */
@@ -101,17 +117,20 @@ public class Pointer implements Comparable<Pointer> {
   //
   // Object methods
   //
-  @Override public boolean equals(final Object that) {
+  @Override
+  public boolean equals(final Object that) {
     return (that instanceof Pointer)
       && ((Pointer) that).source.equals(this.source)
       && ((Pointer) that).index == this.index;
   }
 
-  @Override public int hashCode() {
+  @Override
+  public int hashCode() {
     return source.hashCode() + index;
   }
 
-  @Override public String toString() {
+  @Override
+  public String toString() {
     return new StringBuilder("[Pointer").
       append(" ").
       append(getType().name()).
