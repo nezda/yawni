22ee757608830d49df19114c87d91839f5269f85
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-03 03:27:45 +0000

    - added Apache license
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@519 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/main/java/edu/brandeis/cs/steele/util/Cache.java
+++ core/src/main/java/edu/brandeis/cs/steele/util/Cache.java
@@ -1,12 +1,29 @@
 /*
- * Cache utility class
+ *  Copyright (C) 2007 Google Inc.
  *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+/*
  * Copyright 1998 by Oliver Steele.  You can use this software freely so long as you preserve
  * the copyright notice and this restriction, and label your changes.
  */
 package edu.brandeis.cs.steele.util;
 
-/** A <code>Cache</code> is a collection of values that are indexed by keys and that are stored for an
+/**
+ * A <code>Cache</code> is a collection of values that are indexed by keys and that are stored for an
  * unspecified amount of time (which the implementor of <code>Cache</code> may further specify).
  *
  * @author Oliver Steele, steele@cs.brandeis.edu
