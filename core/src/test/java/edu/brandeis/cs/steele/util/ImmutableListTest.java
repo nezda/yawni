b519c834e2881a28a2bc54ca5d1b651425b6e156
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-03 03:13:39 +0000

    - added Apache license - added iterable() test - @Ignored restleton() test
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@483 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/test/java/edu/brandeis/cs/steele/util/ImmutableListTest.java
+++ core/src/test/java/edu/brandeis/cs/steele/util/ImmutableListTest.java
@@ -1,3 +1,19 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package edu.brandeis.cs.steele.util;
 
 import java.util.ArrayList;
@@ -5,6 +21,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -95,6 +112,7 @@ public class ImmutableListTest {
     assertTrue(resultFromArray instanceof ImmutableList.Quintupleton);
     listTest(result, resultFromArray);
   }
+  @Ignore
   @Test
   public void restleton() {
     System.out.println("restleton()");
@@ -113,6 +131,20 @@ public class ImmutableListTest {
     assertTrue(resultFromArray instanceof ImmutableList.Restleton);
     listTest(result, resultFromArray);
   }
+  @Test
+  public void iterable() {
+    System.out.println("iterable()");
+    Integer[] elements = new Integer[]{ 0, 1, 2, 3, 4};
+    Integer e0 = elements[0];
+    Integer e1 = elements[1];
+    Integer e2 = elements[2];
+    Integer e3 = elements[3];
+    Integer e4 = elements[4];
+    List<Integer> expResult = Collections.unmodifiableList(Arrays.asList(e0, e1, e2, e3, e4));
+    ImmutableList<Integer> result = ImmutableList.of(expResult);
+    assertTrue(result instanceof ImmutableList.Quintupleton);
+    listTest(expResult, result);
+  }
   private <E> void listTest(List<E> expResult, List<E> result) {
     listTest(expResult, result, true);
   }
@@ -125,6 +157,10 @@ public class ImmutableListTest {
       " result class: "+result.getClass().getSimpleName(),
       expResult.toString(), result.toString());
     assertEquals(expResult.hashCode(), result.hashCode());
+    assertTrue(Arrays.equals(expResult.toArray(), result.toArray()));
+    @SuppressWarnings("unchecked")
+    final E[] ref = (E[])new Object[0];
+    assertTrue(Arrays.equals(expResult.toArray(ref), result.toArray(ref)));
     assertTrue(expResult.containsAll(result));
     assertTrue(result.containsAll(expResult));
     for (E e : expResult) {
@@ -137,6 +173,7 @@ public class ImmutableListTest {
       for (int i=0; i <= expResult.size(); i++) {
         listTest(expResult.subList(0, i), result.subList(0, i), false);
       }
+      
       // {0, size()}, {1, size()}, {2,size()}, ..., {size(), size()}
       for (int i = 0; i <= expResult.size(); i++) {
         System.out.println("result class: "+result.getClass().getSimpleName()+
