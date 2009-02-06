1082adcbbccc5dad3b154a1804c72ba2fc51c585
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-06 04:18:40 +0000

    - suppressed bogus warnings
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@536 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/test/java/edu/brandeis/cs/steele/util/MultiLevelIterableTest.java
+++ core/src/test/java/edu/brandeis/cs/steele/util/MultiLevelIterableTest.java
@@ -28,13 +28,16 @@ public class MultiLevelIterableTest {
     final List<List<Integer>> emptyEmpty = new ArrayList<List<Integer>>();
     emptyEmpty.add(Collections.<Integer>emptyList());
     assertEquals(empty, list(MultiLevelIterable.of(emptyEmpty)));
-    final List<Integer> ints1 = Arrays.asList(1,2,3);
-    final List<Integer> ints2 = Arrays.asList(1,2,4);
+    final List<Integer> ints1 = Arrays.asList(1, 2, 3);
+    final List<Integer> ints2 = Arrays.asList(1, 2, 4);
+    @SuppressWarnings("unchecked")
     final List<List<Integer>> ints1Ints2 = Arrays.asList(ints1, ints2);
-    final List<Integer> merged = Arrays.asList(1,2,3,1,2,4);
+    final List<Integer> merged = Arrays.asList(1, 2, 3, 1, 2, 4);
     assertEquals(merged, list(MultiLevelIterable.of(ints1Ints2)));
+    @SuppressWarnings("unchecked")
     final List<List<Integer>> ints1EmptyEmptyInts2 = Arrays.asList(ints1, empty, empty, ints2);
     assertEquals(merged, list(MultiLevelIterable.of(ints1EmptyEmptyInts2)));
+    @SuppressWarnings("unchecked")
     final List<List<Integer>> emptyInts1EmptyEmptyInts2Empty = 
       Arrays.asList(empty, ints1, empty, empty, ints2, empty);
     assertEquals(merged, list(MultiLevelIterable.of(emptyInts1EmptyEmptyInts2Empty)));
