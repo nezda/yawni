221a04803649dc5f74d0c4aca29b97904776c2c3
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-08 20:27:30 +0000

    - use Utils.asList(Iterable)
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@548 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/test/java/edu/brandeis/cs/steele/util/MultiLevelIterableTest.java
+++ core/src/test/java/edu/brandeis/cs/steele/util/MultiLevelIterableTest.java
@@ -18,7 +18,10 @@ package edu.brandeis.cs.steele.util;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+
 import java.util.*;
+
+import static edu.brandeis.cs.steele.util.Utils.asList;
 
 public class MultiLevelIterableTest {
 
@@ -27,27 +30,19 @@ public class MultiLevelIterableTest {
     final List<Integer> empty = Collections.emptyList();
     final List<List<Integer>> emptyEmpty = new ArrayList<List<Integer>>();
     emptyEmpty.add(Collections.<Integer>emptyList());
-    assertEquals(empty, list(MultiLevelIterable.of(emptyEmpty)));
+    assertEquals(empty, asList(MultiLevelIterable.of(emptyEmpty)));
     final List<Integer> ints1 = Arrays.asList(1, 2, 3);
     final List<Integer> ints2 = Arrays.asList(1, 2, 4);
     @SuppressWarnings("unchecked")
     final List<List<Integer>> ints1Ints2 = Arrays.asList(ints1, ints2);
     final List<Integer> merged = Arrays.asList(1, 2, 3, 1, 2, 4);
-    assertEquals(merged, list(MultiLevelIterable.of(ints1Ints2)));
+    assertEquals(merged, asList(MultiLevelIterable.of(ints1Ints2)));
     @SuppressWarnings("unchecked")
     final List<List<Integer>> ints1EmptyEmptyInts2 = Arrays.asList(ints1, empty, empty, ints2);
-    assertEquals(merged, list(MultiLevelIterable.of(ints1EmptyEmptyInts2)));
+    assertEquals(merged, asList(MultiLevelIterable.of(ints1EmptyEmptyInts2)));
     @SuppressWarnings("unchecked")
     final List<List<Integer>> emptyInts1EmptyEmptyInts2Empty = 
       Arrays.asList(empty, ints1, empty, empty, ints2, empty);
-    assertEquals(merged, list(MultiLevelIterable.of(emptyInts1EmptyEmptyInts2Empty)));
-  }
-
-  static <T> ArrayList<T> list(final Iterable<T> sequence) {
-    final ArrayList<T> list = new ArrayList<T>();
-    for (final T t : sequence) {
-      list.add(t);
-    }
-    return list;
+    assertEquals(merged, asList(MultiLevelIterable.of(emptyInts1EmptyEmptyInts2Empty)));
   }
 }
