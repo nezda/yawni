d3d8033e0cb30340b2dcda027bbdf8b58884b2e8
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-08 20:27:51 +0000

    - use Utils.asList(Iterable)
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@549 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/test/java/edu/brandeis/cs/steele/util/MergedIterableTest.java
+++ core/src/test/java/edu/brandeis/cs/steele/util/MergedIterableTest.java
@@ -18,31 +18,34 @@ package edu.brandeis.cs.steele.util;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+
 import java.util.*;
+
+import static edu.brandeis.cs.steele.util.Utils.asList;
 
 public class MergedIterableTest {
 
   @Test
   public void test1() {
     final List<Integer> empty = Collections.emptyList();
-    assertEquals(empty, list(MergedIterable.merge(empty, empty)));
+    assertEquals(empty, asList(MergedIterable.merge(empty, empty)));
     final List<Integer> ints1 = Arrays.asList(1, 2, 3);
     final List<Integer> ints2 = Arrays.asList(1, 2, 4);
     final List<Integer> merged = Arrays.asList(1, 1, 2, 2, 3, 4);
-    assertEquals(merged, list(MergedIterable.merge(ints1, ints2)));
-    assertEquals(merged, list(MergedIterable.merge(true, ints1, ints2)));
+    assertEquals(merged, asList(MergedIterable.merge(ints1, ints2)));
+    assertEquals(merged, asList(MergedIterable.merge(true, ints1, ints2)));
   }
   @Test
   public void test2() {
     final List<Integer> empty = Collections.emptyList();
-    assertEquals(empty, list(MergedIterable.merge(empty, empty)));
+    assertEquals(empty, asList(MergedIterable.merge(empty, empty)));
     final List<Integer> ints1 = Arrays.asList(1, 2, 3);
     final List<Integer> ints2 = Arrays.asList(1, 2, 3);
     final List<Integer> ints3 = Arrays.asList(1, 2, 3);
     final List<Integer> ints4 = Arrays.asList(1, 2, 3);
     final List<Integer> merged = Arrays.asList(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3);
-    assertEquals(merged, list(MergedIterable.merge(ints1, ints2, ints3, ints4)));
-    assertEquals(merged, list(MergedIterable.merge(true, ints1, ints2, ints3, ints4)));
+    assertEquals(merged, asList(MergedIterable.merge(ints1, ints2, ints3, ints4)));
+    assertEquals(merged, asList(MergedIterable.merge(true, ints1, ints2, ints3, ints4)));
   }
   @Test(expected=IllegalArgumentException.class)
   public void testValidateSort1() {
@@ -55,13 +58,5 @@ public class MergedIterableTest {
     final List<Integer> ints1Rev = Arrays.asList(3, 2, 1);
     final List<Integer> ints2 = Arrays.asList(1, 2, 4);
     MergedIterable.merge(true, ints2, ints1Rev);
-  }
-
-  static <T> ArrayList<T> list(final Iterable<T> sequence) {
-    final ArrayList<T> list = new ArrayList<T>();
-    for (final T t : sequence) {
-      list.add(t);
-    }
-    return list;
   }
 }
