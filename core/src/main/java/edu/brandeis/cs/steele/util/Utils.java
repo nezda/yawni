b914dd46a8e21fc68a4d8fe10ae80d83611d68dd
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-08 20:51:01 +0000

    - refactored stuff to new CharSequences - added asList - javadoc
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@567 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/main/java/edu/brandeis/cs/steele/util/Utils.java
+++ core/src/main/java/edu/brandeis/cs/steele/util/Utils.java
@@ -16,6 +16,7 @@
  */
 package edu.brandeis.cs.steele.util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
 import java.util.Comparator;
@@ -26,17 +27,17 @@ public class Utils {
   private Utils() { }
 
   /**
-   * @return <var>str</var> with its first letter {@link Character#toUpperCase}
+   * @return {@code str} with its first letter {@link Character#toUpperCase}
    */
   public static String capitalize(final String str) {
     return Character.toUpperCase(str.charAt(0)) + str.substring(1);
   }
 
   /**
-   * Returns the absolute offset <i>into <var>l1</var></i> where the elements
-   * of sequences <var>l1</var> and <var>l2</var> (with <var>l1</var> starting
-   * at <var>l1s</var> and <var>l2</var> starting at <var>l2s</var>) are <u>first</u>
-   * not <code>equals()</code> or <var>l1e</var> if no such offset exists.
+   * Returns the absolute offset <i>into {@code l1}</i> where the elements
+   * of sequences {@code l1} and {@code l2} (with {@code l1} starting
+   * at {@code l1s} and {@code l2} starting at {@code l2s}) are <u>first</u>
+   * not <code>equals()</code> or {@code l1e} if no such offset exists.
    * Modeled after STL function by the same name, but assumes "random access iterators".
    */
   public static int mismatch(final List<?> l1, int l1s, final int l1e,
@@ -53,7 +54,7 @@ public class Utils {
   /**
    * Equivalent to the STL function by the same name except the
    * <code>first</code> and <code>last</code> are implied to select the entire
-   * contents of <var>iterable</var>.
+   * contents of {@code iterable}.
    * @see <a href="http://www.sgi.com/tech/stl/distance.html">distane</a>
    */
   public static long distance(final Iterable<?> iterable) {
@@ -67,7 +68,7 @@ public class Utils {
   /**
    * @param iterable generator of sequences to check for natural sortedness
    * @return whether or not the naturally <code>Comparable</code> elements of
-   * sequences emitted by <var>iterable</var> are produced in sorted order.
+   * sequences emitted by {@code iterable} are produced in sorted order.
    */
   public static <T extends Object & Comparable<? super T>>
     boolean isSorted(final Iterable<? extends T> iterable) {
@@ -77,9 +78,9 @@ public class Utils {
   /**
    * @param iterable generator of sequences to check for natural sortedness
    * @param infoException if <code>true</code>, throw informative <code>RuntimeException</code>
-   * if <var>iterable</var> isn't sorted
+   * if {@code iterable} isn't sorted
    * @return whether or not the naturally <code>Comparable</code> elements of
-   * sequences emitted by <var>iterable</var> are produced in sorted order.
+   * sequences emitted by {@code iterable} are produced in sorted order.
    */
   public static <T extends Object & Comparable<? super T>>
     boolean isSorted(final Iterable<? extends T> iterable, final boolean infoException) {
@@ -89,7 +90,7 @@ public class Utils {
   /**
    * @param iterator sequence to check for natural sortedness
    * @return whether or not the naturally <code>Comparable</code> elements of
-   * the sequence emitted by <var>iterator</var> are produced in sorted order.
+   * the sequence emitted by {@code iterator} are produced in sorted order.
    */
   public static <T extends Object & Comparable<? super T>>
     boolean isSorted(final Iterator<? extends T> iterator) {
@@ -99,10 +100,10 @@ public class Utils {
   /**
    * @param iterator sequence to check for natural sortedness
    * @param infoException if <code>true</code>, throw informative <code>RuntimeException</code>
-   * if <var>iterator</var> isn't sorted
+   * if {@code iterator} isn't sorted
    * @return whether or not the naturally <code>Comparable</code> elements
-   * emitted by <var>iterator</var> are produced in sorted order.
-   * @throws IllegalArgumentException with informative message if <var>infoException<var>
+   * emitted by {@code iterator} are produced in sorted order.
+   * @throws IllegalArgumentException with informative message if {@code infoException{@code 
    * and <em>not</em> sorted.
    */
   public static <T extends Object & Comparable<? super T>>
@@ -188,8 +189,8 @@ public class Utils {
     }
 
   /**
-   * Removes adjacent duplicates from <var>list</var>.  <b>Assumes</b>
-   * <var>iterable</var>'s items are emitted in sorted order.
+   * Removes adjacent duplicates from {@code list}.  <b>Assumes</b>
+   * {@code iterable}'s items are emitted in sorted order.
    */
   public static <T extends Object & Comparable<? super T>>
     Iterable<T> uniq(final Iterable<T> base) {
@@ -203,8 +204,8 @@ public class Utils {
   }
 
   /**
-   * Removes duplicates from <var>list</var>.  Does <i>not</i> require
-   * <var>list</var> be sorted but does assume <var>list</var>
+   * Removes duplicates from {@code list}.  Does <i>not</i> require
+   * {@code list} be sorted but does assume {@code list}
    * contains no null elements and is short (brute force algorithm).
    */
   public static <T> List<T> dedup(List<T> list) {
@@ -228,7 +229,7 @@ public class Utils {
       }
     }
     if (n < list.size()) {
-      //TODO new list would plug small memory leak here
+      //TODO new List would plug small memory leak here
       list = list.subList(0, n);
     }
     //final Set<T> set = new HashSet<T>(list);
@@ -247,7 +248,7 @@ public class Utils {
     return o1 == o2 || (o1 != null && o1.equals(o2));
   }
 
-  public static class WordNetLexicalComparator implements Comparator<CharSequence> {
+  public static final class WordNetLexicalComparator implements Comparator<CharSequence> {
     // for use with Word lemmas which are all lowercased
     public static final WordNetLexicalComparator GIVEN_CASE_INSTANCE = new WordNetLexicalComparator(false);
     // for use with WordSense lemmas which include natural case information
@@ -258,6 +259,9 @@ public class Utils {
       this.lowerCase = lowerCase;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public int compare(final CharSequence s1, final CharSequence s2) {
       final int s1Len = s1.length();
       final int s2Len = s2.length();
@@ -290,139 +294,16 @@ public class Utils {
       return this == that;
     }
   } // end class WordNetLexicalComparator
+
   
-  /**
-   * @see java.lang.String#hashCode
-   */
-  public static int hashCode(final CharSequence seq) {
-    if (seq instanceof String) {
-      return seq.hashCode();
+  public static <T> List<T> asList(final Iterable<T> iterable) {
+    final List<T> list = new ArrayList<T>();
+    for (final T t : iterable) {
+      list.add(t);
     }
-    // s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]*31^(n-n)
-    // 31 = 2^0 + 2^1 + 2^2 + 2^3 + 2^4 = 2^5 - 1
-    // 2^4 * 2*16
-    int hash = 0, multiplier = 1;
-    for (int i = 0, n = seq.length(); i < n; i++) {
-      hash += seq.charAt(i) * multiplier;
-      int shifted = multiplier << 5;
-      multiplier = shifted - multiplier;
-    }
-    return hash;
+    return list;
   }
-
-  /**
-   * @see java.lang.String#equals
-   */
-  public static boolean equals(final CharSequence s1, final CharSequence s2) {
-    final int s2Len = s2.length();
-    return s1.length() == s2Len && regionMatches(s1, 0, s2, 0, s2Len);
-  }
-
-  /**
-   * @see java.lang.String#startsWith
-   */
-  public static boolean startsWith(final CharSequence s1, final CharSequence s2) {
-    return regionMatches(s1, 0, s2, 0, s2.length());
-  }
-
-  /**
-   * @see java.lang.String#regionMatches
-   */
-  public static boolean regionMatches(final CharSequence s1, int offset1,
-      final CharSequence s2, int offset2, int len) {
-    return regionMatches(false, s1, offset1, s2, offset2, len);
-  }
-
-  /**
-   * @see java.lang.String#regionMatches
-   */
-  public static boolean regionMatches(final boolean ignoreCase, final CharSequence s1, int offset1,
-      final CharSequence s2, int offset2, int len) {
-    //System.err.println("s1: "+s1+" s2: "+s2);
-    final int s1Len = s1.length();
-    //System.err.println("s1Len: "+s1Len);
-    //System.err.println("(s1Len-offset1): "+(s1Len-offset1)+ " len: "+len);
-    if ((s1Len - offset1) < len) {
-      return false;
-    }
-    final int s2Len = s2.length();
-    //System.err.println("s2Len: "+s1Len);
-    //System.err.println("(s2Len-offset2): "+(s2Len-offset2)+ " len: "+len);
-    if ((s2Len - offset2) < len) {
-      return false;
-    }
-    //System.err.println("len: "+len);
-
-    char c1, c2;
-    while (len-- != 0) {
-      c1 = s1.charAt(offset1++);
-      c2 = s2.charAt(offset2++);
-      //System.err.println("c1: "+c1+" c2: "+c2);
-      if (c1 == c2) {
-        continue;
-      }
-      if (ignoreCase) {
-        c1 = Character.toLowerCase(c1);
-        c2 = Character.toLowerCase(c2);
-      }
-      if (c1 == c2) {
-        continue;
-      }
-      return false;
-    }
-    return true;
-  }
-
-  public static boolean containsUpper(final CharSequence string) {
-    int len = string.length();
-    while (len-- != 0) {
-      if (Character.isUpperCase(string.charAt(len))) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /**
-   * A comparison predicate that specifically ignores case and ' ', '.', '-', '_'
-   * letters and their order must still match.
-   */
-  public static boolean sameLetterDigitSequence(final CharSequence s1, final CharSequence s2) {
-    final int s1Len = s1.length();
-    final int s2Len = s2.length();
-    int len = Math.min(s1Len, s2Len);
-    int i = 0, j = 0;
-    char c1, c2;
-    while (len-- != 0) {
-
-    }
-    
-    throw new UnsupportedOperationException("IMPLEMENT ME");
-  }
-
-  //static class CharSequenceComparator implements Comparator<CharSequence>, Serializable {
-  //  private static final long serialVersionUID = 1L;
-  //
-  //  /** {@inheritDoc} */
-  //  public int compare(final CharSequence s1, final CharSequence s2) {
-  //    int i = 0;
-  //    int n = Math.min(s1.length(), s2.length());
-  //    while (n-- != 0) {
-  //      final char c1 = s1.charAt(i);
-  //      final char c2 = s2.charAt(i++);
-  //      if (c1 != c2) {
-  //        return c1 - c2;
-  //      }
-  //    }
-  //    return s1.length() - s2.length();
-  //  }
-  //  /** {@inheritDoc} */
-  //  public boolean equals(final Object obj) {
-  //    return obj instanceof CharSequenceComparator;
-  //  }
-  //  public static final CharSequenceComparator INSTANCE = new CharSequenceComparator();
-  //} // end class CharSequenceComparator
-
+  
   public static <T> T last(final List<T> ts) {
     if (ts == null || ts.isEmpty()) {
       return null;
