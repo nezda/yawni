6c52a73c90ae47e979768028a7c0cceba0f5058e
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-23 02:55:12 +0000

    - rearranged field to make more alignment friendly - javadoc and internal comments
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@590 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/main/java/edu/brandeis/cs/steele/wn/WordSense.java
+++ core/src/main/java/edu/brandeis/cs/steele/wn/WordSense.java
@@ -26,15 +26,18 @@ import java.util.List;
 
 import edu.brandeis.cs.steele.util.CharSequences;
 import edu.brandeis.cs.steele.util.ImmutableList;
-import edu.brandeis.cs.steele.util.Utils;
+import edu.brandeis.cs.steele.util.WordNetLexicalComparator;
 
 /**
- * A <code>WordSense</code> represents the lexical information related to a specific sense of a {@link Word}.
+ * A {@code WordSense} represents the precise lexical information related to a specific sense of a {@link Word}.
  *
- * <code>WordSense</code>'s are linked by {@link Pointer}s into a network of lexically related <code>Synset</code>s
- * and <code>WordSense</code>s.
+ * <p> {@code WordSense}'s are linked by {@link Pointer}s into a network of lexically related {@code Synset}s
+ * and {@code WordSense}s.
  * {@link WordSense#getTargets WordSense.getTargets()} retrieves the targets of these links, and
  * {@link WordSense#getPointers WordSense.getPointers()} retrieves the pointers themselves.
+ *
+ * <p> Each {@code WordSense} has exactly one associtated {@link Word} (however a given {@code Word} may have one
+ * or more {@code WordSense}s with different orthographic case (e.g., the nouns "CD" vs. "Cd").
  *
  * @see Pointer
  * @see Synset
@@ -42,7 +45,7 @@ import edu.brandeis.cs.steele.util.Utils;
  */
 public final class WordSense implements PointerTarget, Comparable<WordSense> {
   /**
-   * <i>Optional</i> restrictions for the position(s) an adjective can take
+   * <em>Optional</em> restrictions for the position(s) an adjective can take
    * relative to the noun it modifies. aka "adjclass".
    */
   public enum AdjPosition {
@@ -71,13 +74,14 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
   // Instance implementation
   //
   private final Synset synset;
+  /** case sensitive lemma */
   private final String lemma;
   private final int lexid;
-  // only needs to be a byte since there are only 3 bits of flag values
-  private final byte flags;
   // represents up to 64 different verb frames are possible (as of now, 35 exist)
   private long verbFrameFlags;
   private short senseNumber;
+  // only needs to be a byte since there are only 3 bits of flag values
+  private final byte flags;
 
   //
   // Constructor
@@ -87,7 +91,7 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
     this.lemma = lemma;
     this.lexid = lexid;
     assert flags < Byte.MAX_VALUE;
-    this.flags = (byte)flags;
+    this.flags = (byte) flags;
     this.senseNumber = -1;
   }
 
@@ -106,7 +110,8 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
     return synset.getPOS();
   }
 
-  /** Returns the natural-cased lemma representation of this <code>WordSense</code>
+  /**
+   * Returns the natural-cased lemma representation of this {@code WordSense}
    * Its lemma is its orthographic representation, for example <tt>"dog"</tt>
    * or <tt>"U.S.A."</tt> or <tt>"George Washington"</tt>.  Contrast to the
    * canonical lemma provided by {@link Word#getLemma()}.
@@ -147,13 +152,16 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
 
   /**
    * 1-indexed value.  Note that this value often varies across WordNet versions.
+   * For those senses which never occured in sense tagged corpora, it is
+   * arbitrarily chosen.
+   * @see <a href="http://wordnet.princeton.edu/man/cntlist.5WN#toc4">'NOTES' in cntlist WordNet documentation</a
    */
   public int getSenseNumber() {
     if (senseNumber < 1) {
-      // Ordering of Word's Synsets (combo(Word, Synset)=Word)
-      // is defined by sense tagged frequency, but this is implicit)
+      // Ordering of Word's Synsets (combo(Word, Synset)=WordSense)
+      // is defined by sense tagged frequency (but this is implicit).
       // Get Word and scan this WordSense's Synsets and 
-      // find the one with this Synset
+      // find the one with this Synset.
       final Word word = getWord();
       int localSenseNumber = 0;
       for (final Synset syn : word.getSynsets()) {
@@ -173,6 +181,7 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
   /** 
    * Use this <code>WordSense</code>'s lemma as key to find its <code>Word</code>.
    */
+  // WordSense contains everything Word does - no need to expose this
   Word getWord() {
     final Word word = synset.fileBackedDictionary.lookupWord(lemma, getPOS());
     assert word != null : "lookupWord failed for \""+lemma+"\" "+getPOS();
@@ -183,6 +192,8 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
    * Build 'sensekey'.  Used for searching <tt>cntlist.rev</tt> and <tt>sents.vrb</tt><br>
    * @see <a href="http://wordnet.princeton.edu/man/senseidx.5WN#sect3">senseidx WordNet documentation</a>
    */
+  //TODO cache this ?does it ever become not useful to cache this ? better to cache getSensesTaggedFrequency()
+  // power users would be into this: https://sourceforge.net/tracker/index.php?func=detail&aid=2009619&group_id=33824&atid=409470
   CharSequence getSenseKey() {
     final String searchWord;
     final int headSense;
@@ -273,6 +284,7 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
     return senseKey;
   }
 
+  //TODO move to unit test
   private String oldAdjClusterSenseKey(final String searchWord, final int headSense) {
     return String.format("%s%%%d:%02d:%02d:%s:%02d",
         getLemma().toLowerCase(),
@@ -283,6 +295,7 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
         headSense);
   }
 
+  //TODO move to unit test
   private String oldNonAdjClusterSenseKey() {
     return String.format("%s%%%d:%02d:%02d::",
         getLemma().toLowerCase(),
@@ -296,11 +309,12 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
    * <a href="http://wordnet.princeton.edu/man/cntlist.5WN.html"><tt>cntlist</tt></a>
    */
   public int getSensesTaggedFrequency() {
-    //TODO cache this value
-    //TODO we could use this Word's getTaggedSenseCount() to determine if
-    //there were any tagged senses for *any* sense of it (including this one)
-    //and really we wouldn't need to look at sense (numbers) exceeding that value
-    //as an optimization
+    // TODO cache this value: requires minimal memory, provides a lot of value
+    // (high-level and eliminating redundant work)
+    // TODO we could use this Word's getTaggedSenseCount() to determine if
+    // there were any tagged senses for *any* sense of it (including this one)
+    // and really we wouldn't need to look at sense (numbers) exceeding that value
+    // as an optimization
     final CharSequence senseKey = getSenseKey();
     final FileBackedDictionary dictionary = synset.fileBackedDictionary;
     final String line = dictionary.lookupCntlistDotRevLine(senseKey);
@@ -366,13 +380,22 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
   }
 
   /**
-   * <p>Returns illustrative sentences and generic verb frames.  This <b>only</b> has values
+   * <p> Returns illustrative sentences <em>and</em> generic verb frames.  This <b>only</b> has values
    * for {@link POS#VERB} <code>WordSense</code>s.
    *
-   * <p>For illustrative sentences (<tt>sents.vrb</tt>), "%s" is replaced with the verb lemma
+   * <p> For illustrative sentences (<tt>sents.vrb</tt>), "%s" is replaced with the verb lemma
    * which seems unnecessarily ineficient since you have the WordSense anyway.
    *
-   * <p>Official WordNet 3.0 documentation indicates "specific" and generic frames
+   * <pre>{@literal Example: verb "complete"#1 has 4 generic verb frames:
+   *    1. *> Somebody ----s
+   *    2. *> Somebody ----s something
+   *    3. *> Something ----s something
+   *    4. *> Somebody ----s VERB-ing
+   *  and 1 specific verb frame:
+   *    1. EX: They won't %s the story
+   * }</pre>
+   *
+   * <p> Official WordNet 3.0 documentation indicates "specific" and generic frames
    * are mutually exclusive which is not the case.
    *
    * @see <a href="http://wordnet.princeton.edu/man/wndb.5WN#sect6">http://wordnet.princeton.edu/man/wndb.5WN#sect6</a>
@@ -554,7 +577,7 @@ public final class WordSense implements PointerTarget, Comparable<WordSense> {
    */
   public int compareTo(final WordSense that) {
     int result;
-    result = Utils.WordNetLexicalComparator.TO_LOWERCASE_INSTANCE.compare(this.getLemma(), that.getLemma());
+    result = WordNetLexicalComparator.TO_LOWERCASE_INSTANCE.compare(this.getLemma(), that.getLemma());
     if (result == 0) {
       result = this.getSenseNumber() - that.getSenseNumber();
       if (result == 0) {
