bf3b91a07c5e60121b402bed67a15de93f2504d0
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-24 03:51:17 +0000

    convert to slf4j
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@628 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/main/java/edu/brandeis/cs/steele/util/InfoOnlyFormatter.java
+++ core/src/main/java/edu/brandeis/cs/steele/util/InfoOnlyFormatter.java
@@ -16,24 +16,30 @@
  */
 package edu.brandeis.cs.steele.util;
 
-import java.util.logging.*;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
 /**
  */
 public final class InfoOnlyFormatter extends SimpleFormatter {
-  @Override public String format(final LogRecord record) {
+  @Override
+  public String format(final LogRecord record) {
     return new StringBuilder().
       append(String.format("%-7s", record.getLevel())).
-      append(" ").
-      //append(record.getSourceClassName()).append(" ").
-      append(simplifyClassName(record.getSourceClassName())).append(".").
+      append(' ').
+      //append(record.getSourceClassName()).append(' ').
+      append(simplifyClassName(record.getSourceClassName())).append('.').
       append(record.getSourceMethodName()).append("() ").
       append(record.getMessage()).
-      append("\n").toString();
+      append('\n').toString();
   }
-  @Override public String getHead(final Handler h) { return ""; }
-  @Override public String getTail(final Handler h) { return ""; }
+  @Override
+  public String getHead(final Handler h) { return ""; }
+  @Override
+  public String getTail(final Handler h) { return ""; }
   static String simplifyClassName(String fullClassName) {
-    final int idx = fullClassName.lastIndexOf(".");
+    final int idx = fullClassName.lastIndexOf('.');
     return fullClassName.substring(idx + 1);
   }
 }
