814ddd675de8237757f50d369a58d3ec32648360
nezda <nezda@158bccb4-7c0b-4fb3-968f-7c40bf2d9062>
2009-02-24 03:02:19 +0000

    javadoc
    git-svn-id: file:///Users/nezda/code/svn/jwordnet@618 158bccb4-7c0b-4fb3-968f-7c40bf2d9062

--- core/src/main/java/edu/brandeis/cs/steele/wn/DictionaryDatabase.java
+++ core/src/main/java/edu/brandeis/cs/steele/wn/DictionaryDatabase.java
@@ -30,30 +30,30 @@ import java.util.List;
  */
 public interface DictionaryDatabase {
   /**
-   * Look up a <code>Word</code> in the database by its <b>lemma</b>.  The search is
+   * Look up a {@code Word} in the database by its <b>lemma</b>.  The search is
    * case-independent and phrases are separated by spaces (e.g., "look up", not
    * "look_up"), but otherwise {@code lemma}  must match the form in the
-   * database exactly.  Similar to C function <code>index_lookup</code>.
+   * database exactly.  Similar to C function {@code index_lookup}.
    * @param lemma The orthographic representation of the word.
    * @param pos The part-of-speech.
-   * @return An <code>Word</code> representing the word, or
-   * <code>null</code> if no such entry exists.
+   * @return An {@code Word} representing the word, or
+   * {@code null} if no such entry exists.
    */
   public Word lookupWord(final CharSequence lemma, final POS pos);
 
   ///**
   // * Return the base form of an exceptional derivation, if an entry for it
   // * exists in the database. e.g., returns "goose" from derivation query term
-  // * "geese" as <code>POS.NOUN</code>.
+  // * "geese" as {@code POS.NOUN}.
   // * @param pos The part-of-speech.
   // * @param derivationLemma A (possibly <i>inflected</i>) form of the word.
-  // * @return The <i>uninflected</i> word, or <code>null</code> if no exception entry exists.
+  // * @return The <i>uninflected</i> word, or {@code null} if no exception entry exists.
   // */
   //public String lookupBaseForm(final POS pos, final String derivationLemma);
 
   /**
    * Return all base forms (aka "lemmas", "stems") of {@code someString} in {@code pos}.
-   * Utilizes an implementation of the <code>morphstr()</code> and <code>getindex()</code> algorithms.
+   * Utilizes an implementation of the {@code morphstr()} and {@code getindex()} algorithms.
    * @param someString
    * @param pos The part-of-speech.
    * @return baseform(s) of {@code someString}
@@ -62,74 +62,74 @@ public interface DictionaryDatabase {
   public List<String> lookupBaseForms(final String someString, final POS pos);
 
   /**
-   * Convenient combination of basic API methods <code>lookupBaseForms()</code>, <code>lookupWord()</code>
-   * and <code>Word.getSynsets()</code>.
+   * Convenient combination of basic API methods {@code lookupBaseForms()}, {@code lookupWord()}
+   * and {@code Word.getSynsets()}.
    * @param someString
    * @param pos The part-of-speech.
-   * @return <code>Synset</code>(s) of {@code someString} in {@code pos}
+   * @return {@code Synset}(s) of {@code someString} in {@code pos}
    */
   public List<Synset> lookupSynsets(final String someString, final POS pos);
 
   /**
-   * Return an iterator of <b>all</b> the <code>Word</code>s in the database.
+   * Return an iterator of <b>all</b> the {@code Word}s in the database.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Word</code>s.
+   * @return An iterable of {@code Word}s.
    */
   public Iterable<Word> words(final POS pos);
 
   /**
-   * Return an iterator of all the <code>Word</code>s whose <em>lemmas</em> contain {@code substring}
+   * Return an iterator of all the {@code Word}s whose <em>lemmas</em> contain {@code substring}
    * as a <b>substring</b>.
    * @param substring The substring to search for.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Word</code>s.
+   * @return An iterable of {@code Word}s.
    */
   public Iterable<Word> searchBySubstring(final CharSequence substring, final POS pos);
 
   /**
-   * Return an iterator of all the <code>Word</code>s whose <em>lemmas</em> <b>begin with</b> {@code prefix}.
+   * Return an iterator of all the {@code Word}s whose <em>lemmas</em> <b>begin with</b> {@code prefix}.
    * @param prefix The prefix to search for.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Word</code>s.
+   * @return An iterable of {@code Word}s.
    */
   public Iterable<Word> searchByPrefix(final CharSequence prefix, final POS pos);
 
   /**
-   * Return an iterator of all the <code>Synset</code>s whose gloss <b>contains</b> {@code substring}
+   * Return an iterator of all the {@code Synset}s whose gloss <b>contains</b> {@code substring}
    * <em>without stemming</em> (i.e., {@link #lookupBaseForms()}).
    * @param substring The substring to search for.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Synset</code>s.
+   * @return An iterable of {@code Synset}s.
    */
   public Iterable<Synset> searchGlossBySubstring(final CharSequence substring, final POS pos);
 
   /**
-   * Return an iterator of <b>all</b> the <code>Synset</code>s in the database.
+   * Return an iterator of <b>all</b> the {@code Synset}s in the database.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Synset</code>s.
+   * @return An iterable of {@code Synset}s.
    */
   public Iterable<Synset> synsets(final POS pos);
 
   /**
-   * Return an iterator of <b>all</b> the <code>WordSense</code>s in the database.
+   * Return an iterator of <b>all</b> the {@code WordSense}s in the database.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>WordSense</code>s.
+   * @return An iterable of {@code WordSense}s.
    */
   public Iterable<WordSense> wordSenses(final POS pos);
 
   /**
-   * Return an iterator of <b>all</b> the <code>Pointer</code>s in the database.
+   * Return an iterator of <b>all</b> the {@code Pointer}s in the database.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Pointer</code>s.
+   * @return An iterable of {@code Pointer}s.
    */
   public Iterable<Pointer> pointers(final POS pos);
 
   /**
-   * Return an iterator of <b>all</b> the <code>Pointer</code>s in the database of
-   * type <code>PointerType</code>.
-   * @param pointerType The <code>PointerType</code>. <code>null</code> implies ALL <code>PointerType</code>s.
+   * Return an iterator of <b>all</b> the {@code Pointer}s in the database of
+   * type {@code PointerType}.
+   * @param pointerType The {@code PointerType}. {@code null} implies ALL {@code PointerType}s.
    * @param pos The part-of-speech.
-   * @return An iterable of <code>Pointer</code>s of type <code>PointerType</code>.
+   * @return An iterable of {@code Pointer}s of type {@code PointerType}.
    */
   public Iterable<Pointer> pointers(final PointerType pointerType, final POS pos);
 }
