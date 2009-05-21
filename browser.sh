d974b256f76f63659b313a7564333c6e0cb38c5a
Luke Nezda <lnezda@users.sourceforge.net>
2009-05-21 07:53:11 -0500

    jwordnet to yawni

--- browser.sh
+++ browser.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # Run the browser
+#set -v
 VERSION=2.0.0-SNAPSHOT
 #LEVEL_ARG=-Dedu.brandeis.cs.steele.wn.Morphy.level=SEVERE
-#ASSERT_ENABLE="-ea -esa"
 # system assertions cause tons of logging on OS X ([AWT-\d+] ...)
-#ASSERT_ENABLE="-ea -Xrunhprof:cpu=samples,depth=20,interval=20"
 #ASSERT_ENABLE="-ea"
-#-Dswing.aatext=true
+#ASSERT_ENABLE="-ea -esa"
+#ASSERT_ENABLE="-ea -Xrunhprof:cpu=samples,depth=20,interval=20"
 # the OS X brushMetal* stuff only seems to work if provided as a system property at the command line
 ASSERT_ENABLE=" -ea -Dswing.aatext=true -Dawt.useSystemAAFontSettings=on -Dapple.awt.textantialiasing=on -Dapple.laf.useScreenMenuBar=true -Dapple.awt.brushMetalLook=true -Dapple.awt.brushMetalRounded=true -Dapple.awt.showGrowBox=false"
 #java='/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin/java -ea'
-unset WNHOME
 java='java'
-#$java -Xdock:name="JWordNet Browser" $LEVEL_ARG $ARGS -Dfile.encoding=US-ASCII $ASSERT_ENABLE -DWNHOME="$WNHOME" -cp ./dict:./core/target/jwordnet-core-$VERSION.jar edu.brandeis.cs.steele.wn.browser.Browser "$@"
-CLASSPATH=./core/target/jwordnet-core-$VERSION.jar:./data/target/jwordnet-data-$VERSION.jar
-$java -Xdock:name="JWordNet Browser" $LEVEL_ARG $ARGS -Dfile.encoding=US-ASCII $ASSERT_ENABLE -DWNHOME="$WNHOME" -cp "$CLASSPATH" edu.brandeis.cs.steele.wn.browser.Browser "$@"
+# read data from jar
+#unset WNHOME
+wnhome=$WNHOME
+unset WNHOME
+#CLASSPATH=./core/target/jwordnet-core-$VERSION.jar:./data/target/jwordnet-data-$VERSION.jar
+echo "WNHOME: $WNHOME wnhome: $wnhome";
+MVN_REPO=$HOME/.m2/repository/
+CLASSPATH=$MVN_REPO/org/yawni/wn/yawni-core/$VERSION/yawni-core-$VERSION.jar:$MVN_REPO/org/slf4j/slf4j-api/1.5.6/slf4j-api-1.5.6.jar:$MVN_REPO/org/slf4j/slf4j-jdk14/1.5.6/slf4j-jdk14-1.5.6.jar
+#CLASSPATH=$CLASSPATH:$MVN_REPO/org/yawni/wn/yawni-data/$VERSION/yawni-data-$VERSION.jar
+JVM_ARGS="-Xdock:name=\"Yawni Browser\" -Dfile.encoding=US-ASCII"
+$java -Xdock:name="Yawni Browser" -Dfile.encoding=US-ASCII $ASSERT_ENABLE -DWNHOME="$wnhome" -cp "$CLASSPATH" org.yawni.wn.browser.Browser "$@"
+#$java -Xdock:name="Yawni Browser" -Dfile.encoding=US-ASCII $ASSERT_ENABLE -cp "$CLASSPATH" org.yawni.wn.browser.Browser "$@"
