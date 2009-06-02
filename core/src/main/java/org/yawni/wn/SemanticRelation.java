314bb4898c386e030d2b3c904bafbe13f38562ef
Luke Nezda <lnezda@users.sourceforge.net>
2009-06-01 23:25:12 -0500

    sanity assert

--- core/src/main/java/org/yawni/wn/SemanticRelation.java
+++ core/src/main/java/org/yawni/wn/SemanticRelation.java
@@ -27,6 +27,7 @@ public final class SemanticRelation extends Relation {
   SemanticRelation(final int targetOffset, final int targetIndex, final byte targetPOSOrdinal,
     final int index, final RelationTarget source, final byte relationTypeOrdinal) {
     super(targetOffset, targetIndex, targetPOSOrdinal, index, source, relationTypeOrdinal);
+    assert getType().isSemantic();
   }
 
   @Override
