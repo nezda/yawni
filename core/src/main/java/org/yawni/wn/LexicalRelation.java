5233d925ea5a47e27d0da4d332f26f65f46110a7
Luke Nezda <lnezda@users.sourceforge.net>
2009-06-08 08:06:51 -0500

    fixed infinite recursion

--- core/src/main/java/org/yawni/wn/LexicalRelation.java
+++ core/src/main/java/org/yawni/wn/LexicalRelation.java
@@ -27,7 +27,8 @@ public final class LexicalRelation extends Relation {
   LexicalRelation(final int targetOffset, final int targetIndex, final byte targetPOSOrdinal,
     final int index, final RelationTarget source, final byte relationTypeOrdinal) {
     super(targetOffset, targetIndex, targetPOSOrdinal, index, source, relationTypeOrdinal);
-    assert getType().isLexical();
+    assert getSource() instanceof WordSense;
+    // can't call getTarget() - infinite recursion
   }
 
   @Override
