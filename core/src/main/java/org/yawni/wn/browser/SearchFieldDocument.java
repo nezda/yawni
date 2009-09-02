386a0d3dc0d199fb1769da735024f68ab27beeac
Luke Nezda <lnezda@users.sourceforge.net>
2009-09-01 23:34:11 -0500

    cleanup, filter loan slash (likely for nav anyway)

--- core/src/main/java/org/yawni/wn/browser/SearchFieldDocument.java
+++ core/src/main/java/org/yawni/wn/browser/SearchFieldDocument.java
@@ -17,7 +17,6 @@
 package org.yawni.wn.browser;
 
 import javax.swing.text.*;
-import java.util.regex.*;
 
 /**
  * java.swing.text.Document to constrain a search field
@@ -26,8 +25,6 @@ import java.util.regex.*;
 class SearchFieldDocument extends PlainDocument {
   private static final long serialVersionUID = 1L;
 
-  private Pattern pattern;
-  private Matcher matcher;
   private final int MAX_LEN = 256;
 
   @Override
@@ -54,12 +51,12 @@ class SearchFieldDocument extends PlainDocument {
     // TODO scrub text and then insert the remains
     // shortcut common case: is short and contains nothing but letters or digits
     boolean dirty = false == isClean(text, insOffset);
-    if(dirty) {
+    if (dirty) {
       //System.err.println("dirty insert?: \""+text+"\"");
       text = scrub(text, insOffset);
     }
     super.insertString(insOffset, text, attr);
-    if(dirty) {
+    if (dirty) {
       //System.err.println("dirty doc: "+getText(0, getLength()));
     }
   }
@@ -70,41 +67,46 @@ class SearchFieldDocument extends PlainDocument {
       final int length,
       String text,
       final AttributeSet attr) throws BadLocationException {
-    if(length != 0 && false == isClean(text, offset)) {
-      final String toReplace = getText(offset, length);
+    if (length != 0 && false == isClean(text, offset)) {
+      //final String toReplace = getText(offset, length);
       //System.err.println("toReplace: \""+toReplace+"\""+
       //    " (length: "+length+") with \""+text+"\" (length: "+text.length()+")");
       text = scrub(text, offset);
     }
-    super.replace(offset, length, text, attr);
+    //System.err.printf("length: %s text: \"%s\"\n", length, text);
+    if (text != null && text.length() != 0) {
+      // prevent slash when text is selected from deleting word
+      super.replace(offset, length, text, attr);
+    }
   }
 
   // allow all inserts to run, just some wth empty strings
   private String scrub(final String proposedNewText, final int insOffset) {
-    final int currDocLen = getLength();
-    final String prevText;
-    final String nextText;
-    try {
-      prevText = getText(0, insOffset);
-      nextText = currDocLen == insOffset ? "" :
-        getText(insOffset, currDocLen - insOffset);
-    } catch(BadLocationException ble) {
-      throw new RuntimeException(ble);
-    }
-    final int lastChar = prevText.length() != 0 ? prevText.charAt(0) : 0;
+//    final int currDocLen = getLength();
+//    final String prevText;
+//    final String nextText;
+//    try {
+//      prevText = getText(0, insOffset);
+//      nextText = currDocLen == insOffset ? "" :
+//        getText(insOffset, currDocLen - insOffset);
+//    } catch (BadLocationException ble) {
+//      throw new RuntimeException(ble);
+//    }
+//    final int lastChar = prevText.length() != 0 ? prevText.charAt(0) : 0;
     String toInsert = proposedNewText;
     // - bound total document length to MAX_LEN
     toInsert = toInsert.substring(0, Math.min(toInsert.length(), MAX_LEN));
+    // ignore navigation-only slash
+    toInsert = toInsert.replace("/", "");
     // - normalize any internal whitespace
     //   - corner case: allow it to end with up to 1 space or hyphen
-
     return toInsert;
   }
 
   private boolean isClean(final String proposedNewText, final int insOffset) {
-    for(int i = 0, len = proposedNewText.length();
+    for (int i = 0, len = proposedNewText.length();
         i < len; i++) {
-      if(false == Character.isLetterOrDigit(proposedNewText.charAt(i))) {
+      if (false == Character.isLetterOrDigit(proposedNewText.charAt(i))) {
         return false;
       }
     }
