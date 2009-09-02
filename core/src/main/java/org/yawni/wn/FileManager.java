a8a25001a85b3ffae920a8cff05c75256aa1aab8
Luke Nezda <lnezda@users.sourceforge.net>
2009-09-01 23:29:24 -0500

    moved copyright to NOTICE.txt

--- core/src/main/java/org/yawni/wn/FileManager.java
+++ core/src/main/java/org/yawni/wn/FileManager.java
@@ -14,10 +14,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-/*
- * Copyright 1998 by Oliver Steele.  You can use this software freely so long as you preserve
- * the copyright notice.
- */
 package org.yawni.wn;
 
 import org.yawni.util.CharSequences;
