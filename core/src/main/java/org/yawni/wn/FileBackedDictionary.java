15fa5053db0c7c6de9f9ff1a284209cc98fabfab
Luke Nezda <lnezda@users.sourceforge.net>
2009-10-26 22:01:26 -0500

    unused import

--- core/src/main/java/org/yawni/wn/FileBackedDictionary.java
+++ core/src/main/java/org/yawni/wn/FileBackedDictionary.java
@@ -35,7 +35,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
