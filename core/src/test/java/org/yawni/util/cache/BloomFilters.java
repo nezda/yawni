61734f689ba7cfb28e2713a0374d1b05234c3b0e
Luke Nezda <lnezda@users.sourceforge.net>
2009-10-26 21:09:41 -0500

    Apache license header, javadoc

--- core/src/test/java/org/yawni/util/cache/BloomFilters.java
+++ core/src/test/java/org/yawni/util/cache/BloomFilters.java
@@ -1,3 +1,19 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.yawni.util.cache;
 
 import java.io.BufferedInputStream;
@@ -12,6 +28,11 @@ import org.yawni.wn.FileBackedDictionary;
 import org.yawni.wn.POS;
 import org.yawni.wn.Word;
 
+/**
+ * Utility class to generate and serialized BloomFilters representing the
+ * content of a given WordNet version.  These are optionally
+ * packaged in the data jar.
+ */
 class BloomFilters {
   public static void main(String[] args) throws Exception {
     final double fpProb = 0.001;
