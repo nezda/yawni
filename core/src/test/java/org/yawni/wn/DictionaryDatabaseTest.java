7fca66f11502c719019b3cfd44859147b772c0db
Luke Nezda <lnezda@users.sourceforge.net>
2009-11-15 21:50:16 -0600

    minor

--- core/src/test/java/org/yawni/wn/DictionaryDatabaseTest.java
+++ core/src/test/java/org/yawni/wn/DictionaryDatabaseTest.java
@@ -20,9 +20,6 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.*;
 
-/**
- * 
- */
 public class DictionaryDatabaseTest {
   private static DictionaryDatabase dictionary;
   @BeforeClass
