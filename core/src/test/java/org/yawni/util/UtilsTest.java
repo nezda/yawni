2d65f936789466ca509b03fa88ca1a999a046dea
Luke Nezda <lnezda@users.sourceforge.net>
2010-01-24 22:50:47 -0600

    imports

--- core/src/test/java/org/yawni/util/UtilsTest.java
+++ core/src/test/java/org/yawni/util/UtilsTest.java
@@ -16,16 +16,14 @@
  */
 package org.yawni.util;
 
-import org.yawni.util.CharSequences;
-import org.yawni.util.Utils;
-import org.junit.*;
+import java.nio.CharBuffer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.nio.CharBuffer;
-import java.util.*;
-
 public class UtilsTest {
-
   @Test
   public void testEquals() {
     Integer i1 = null;
