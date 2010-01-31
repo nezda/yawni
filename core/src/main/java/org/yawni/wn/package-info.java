59d0ad53cee4d5c95fc51c134d47cfad67f2bc81
Luke Nezda <lnezda@users.sourceforge.net>
2010-01-31 15:35:41 -0600

    javadoc fix

--- core/src/main/java/org/yawni/wn/package-info.java
+++ core/src/main/java/org/yawni/wn/package-info.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.yawni.wn;
 /**
  * Key package for all Yawni classes.
  */
+package org.yawni.wn;
