3fbf9d8564e9f6b8af15b1ffbbb2a8e03f08d235
Luke Nezda <lnezda@users.sourceforge.net>
2010-02-03 21:31:43 -0600

    moved

---
+++ core/src/test/java/org/yawni/util/WordCaseUtilsTest.java
@@ -0,0 +1,64 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.yawni.util;
+
+import org.yawni.util.WordCaseUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.yawni.wn.DictionaryDatabase;
+import org.yawni.wn.FileBackedDictionary;
+import org.yawni.wn.POS;
+import org.yawni.wn.Word;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class WordCaseUtilsTest {
+  private DictionaryDatabase dictionary;
+  @Before
+  public void init() {
+    dictionary = FileBackedDictionary.getInstance();
+  }
+
+  @Test
+  public void test() {
+    String lemma;
+    Word word;
+    lemma = "CD";
+    word = getWord(lemma, POS.NOUN);
+    System.err.println("case variants: "+WordCaseUtils.getUniqueLemmaCaseVariants(word));
+    System.err.println("dominant case: "+WordCaseUtils.getDominantCasedLemma(word));
+    assertThat(WordCaseUtils.getUniqueLemmaCaseVariants(word)).containsExactly("Cd", "cd", "CD");
+    assertThat(WordCaseUtils.getDominantCasedLemma(word)).isEqualTo("cd");
+
+    lemma = "roma";
+    word = getWord(lemma, POS.NOUN);
+    System.err.println("case variants: "+WordCaseUtils.getUniqueLemmaCaseVariants(word));
+    System.err.println("dominant case: "+WordCaseUtils.getDominantCasedLemma(word));
+    assertThat(WordCaseUtils.getUniqueLemmaCaseVariants(word)).containsExactly("Roma");
+    assertThat(WordCaseUtils.getDominantCasedLemma(word)).isEqualTo("Roma");
+    
+    lemma = "rom";
+    word = getWord(lemma, POS.NOUN);
+    System.err.println("case variants: "+WordCaseUtils.getUniqueLemmaCaseVariants(word));
+    System.err.println("dominant case: "+WordCaseUtils.getDominantCasedLemma(word));
+    assertThat(WordCaseUtils.getUniqueLemmaCaseVariants(word)).containsExactly("ROM");
+    assertThat(WordCaseUtils.getDominantCasedLemma(word)).isEqualTo("ROM");
+  }
+
+  private Word getWord(final String lemma, final POS pos) {
+    return dictionary.lookupWord(lemma, pos);
+  }
+}
