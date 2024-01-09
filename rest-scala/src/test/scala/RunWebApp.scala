0e81b46f5ad3cf231f5137a4f351987bc609b983
Luke Nezda <lnezda@gmail.com>
2024-01-09 07:43:25 -0600

    experimenting

--- rest-scala/src/test/scala/RunWebApp.scala
+++ rest-scala/src/test/scala/RunWebApp.scala
@@ -4,6 +4,8 @@ import org.eclipse.jetty.webapp.WebAppContext
 
 object RunWebApp extends App {
   val server = new Server
+  // https://xy2401.com/local-docs/java/jetty.9.4.24.v20191120/session-configuration-housekeeper.html
+  // no effect server.setAttribute("jetty.sessionIdManager.workerName", "yawni0")
   val http = new ServerConnector(server)
   http.setPort(8080)
   server.setConnectors(Array(http))
@@ -16,18 +18,23 @@ object RunWebApp extends App {
 
   server.setHandler(context)
 
-  try {
-    println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
-    server.start()
-    while (System.in.available() == 0) {
-      Thread.sleep(5000)
-    }
-    server.stop()
-    server.join()
-  } catch {
-    case exc : Exception => {
-      exc.printStackTrace()
-      System.exit(100)
-    }
-  }
+  server.start()
+  // Keep the main thread alive while the server is running.
+  //  XXX needed? from https://happycoding.io/tutorials/java-server/embedded-jetty
+  server.join()
+
+//  try {
+//    println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
+//    server.start()
+//    while (System.in.available() == 0) {
+//      Thread.sleep(5000)
+//    }
+//    server.stop()
+//    server.join()
+//  } catch {
+//    case exc : Exception => {
+//      exc.printStackTrace()
+//      System.exit(100)
+//    }
+//  }
 }
